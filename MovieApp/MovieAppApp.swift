325b204ebfef67746bd35178ecd19a2a85b6d2a9
Harshal <harshalpp441@gmail.com>
2026-08-15 13:11:14 +0530

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Harshal Patankar on 15/08/26.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
